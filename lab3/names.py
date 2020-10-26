5285c3dd6b2c85e65b330abbd72be4b4ba600f03
Irina Redkina <irisha132132@mail.ru>
2020-10-26 15:51:30 +0300

    lab3: fix top and error lab4 init

--- lab3/names.py
+++ lab3/names.py
@@ -8,7 +8,11 @@ man = {}
 # Вход: nameYYYY.html, Выход: список начинается с года, продолжается имя-ранг в алфавитном порядке.
 # '2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' и т.д.
 def extr_name(filename):
-    file = et.parse(filename)
+    try:
+        file = et.parse(filename)
+    except OSError:
+        print('File ' + filename + 'not found! Skipped')
+        return
     year = file.xpath('/html/body/table/tr/td/form/input')[0].value
     allTable = file.xpath('/html/body/table/tr/td/table/tr')
     allPeople = []
@@ -18,11 +22,11 @@ def extr_name(filename):
             if woman.get(row[2].text) is None:
                 woman[row[2].text] = int(row[0].text)
             else:
-                woman[row[2].text] += int(row[0].text)
+                woman[row[2].text] = int((int(row[0].text) + int(woman[row[2].text])) / 2)
             if man.get(row[1].text) is None:
                 man[row[1].text] = int(row[0].text)
             else:
-                man[row[1].text] += int(row[0].text)
+                man[row[1].text] = int((int(row[0].text) + int(man[row[1].text])) / 2)
             allPeople.append((row[0].text, row[2].text))
             allPeople.append((row[0].text, row[1].text))
     print_list(allPeople, year)
@@ -32,7 +36,7 @@ def extr_name(filename):
 # напечатать ТОП-10 муж и жен имен из всех переданных файлов
 def get_top(table_list):
     i = 0
-    table_list = sorted(table_list.items(), key=lambda item: item[1], reverse=True)
+    table_list = sorted(table_list.items(), key=lambda item: item[1])
     for person in table_list:
         if i < 10:
             print(person[0])
